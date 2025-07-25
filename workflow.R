10a631dbdebe0aaa77f53f35f1ec5740876eb4c5
Kelly McCain <kem22@dide.local>
2025-07-25 17:05:42 +0100

    update workflow with new rainfall script

--- workflow.R
+++ workflow.R
@@ -8,6 +8,8 @@ orderly_run(name = 'clean_trial_data')
 orderly_run(name = 'trial_results')
 
 # Model 
+orderly_run(name = 'fit_rainfall')
+
 orderly_run(name = 'run_smc_rtss',
             parameters = list(
               n_particles = 50,
@@ -17,23 +19,20 @@ orderly_run(name = 'run_smc_rtss',
               tstep = 1,
               max_SMC_kill_rate = 10,
               SMC_decay = 0.05,
-              season_start_day = 0,
-              season_length = 120,
+              season_start_day = 30,
+              season_length = 150,
               smc_interval = 30,
               inf_start = 0
             ))
 
 
 orderly_run(name = 'sim_cohort',
-            parameters = list(N = 2000, # size of cohort population 
-                              trial_ts = 365*3, # trial timesteps in cohort simulation 
-                              burnin = 50, # number of days in cohort simulation to use for burnin
-                              max_SMC_kill_rate = 8,
-                              SMC_decay = 0.05,
-                              season_start_day = 183,#213,
-                              season_length = 120,
-                              smc_interval = 30,
-                              vax_day = -10 # day of vaccination relative to start of follow-up (- means before follow-up)
+            parameters = list(N = 2000#, # size of cohort population 
+                              # max_SMC_kill_rate = 8,
+                              # SMC_decay = 0.05,
+                              # season_start_day = 50,#213,
+                              # season_length = 30*5,
+                              # smc_interval = 30
             ))
 
 # process_model_output - formatting the data to match the output from the trial 
