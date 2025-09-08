ba175cf82765eeaee0ba364a2539f48c8ac3151d
Kelly McCain <kem22@dide.local>
2025-09-08 14:44:30 +0100

    allow superinfections as parameter; add prob of bite to parameters df; update susceptibility vector depending on if children were infected or not and add in booster timing

--- src/sim_cohort_grid/cohort_sim_utils.R
+++ src/sim_cohort_grid/cohort_sim_utils.R
@@ -1,9 +1,11 @@
 # functions to do parameter sweeps 
 
 # Main cohort simulation function
-run_cohort_simulation <- function(params_row, 
+run_cohort_simulation <- function(metadata_df, 
+                                  params_row, 
                                   base_inputs, 
                                   output_dir = 'simulation_outputs',
+                                  allow_superinfections = TRUE, 
                                   save_outputs = TRUE) {
   
   # Extract parameters from the row
@@ -11,13 +13,17 @@ run_cohort_simulation <- function(params_row,
   SMC_decay <- params_row$SMC_decay
   season_start_day <- params_row$season_start_day
   season_length <- 30*5#params_row$season_length
+  lag_p_bite <- params_row$lag_p_bite
+  # get specific probability of bite for each run 
+  p_bite_bfa <- unlist(params_row$p_bite_bfa)
+  p_bite_mli <- unlist(params_row$p_bite_mli)
   
   # Create unique simulation ID
-  sim_id <- paste0("sim_", 
+  sim_id <- paste0("sim_",
                    "SMCkill", max_SMC_kill_rate,
                    "_SMCdecay", SMC_decay,
-                   "_season", season_start_day,
-                   "_length", season_length)
+                   "_season", season_start_day)#,
+                   # "_lag")
   
   message("Running simulation: ", sim_id)
   
@@ -25,8 +31,6 @@ run_cohort_simulation <- function(params_row,
   N <- base_inputs$N_children
   trial_ts <- base_inputs$trial_timesteps#[[i]]
   burnin <- base_inputs$burnin
-  p_bite_bfa <- base_inputs$p_bite_bfa
-  p_bite_mli <- base_inputs$p_bite_mli
   threshold <- base_inputs$threshold
   VB <- base_inputs$VB
   tstep <- base_inputs$tstep
@@ -35,6 +39,7 @@ run_cohort_simulation <- function(params_row,
   vax_day <- base_inputs$vax_day
   country <- base_inputs$country
   smc_interval <- base_inputs$smc_interval
+  country_to_run <- base_inputs$country
   
   # Initialize storage for this simulation
   infection_records <- data.frame(
@@ -46,31 +51,40 @@ run_cohort_simulation <- function(params_row,
     PEV = integer(),
     SMC = integer(),
     arm = character(),
-    p_bite = double()
+    p_bite = double(),
+    recovery_day = double()
   )
   
   # Create child metadata 
   # when i simulate the real cohort, this will be more specific to the information in the data from the trial 
-  metadata_child <- data.frame(
-    rid = 1:N,
-    vaccination_day = vax_day,
-    PEV = rbinom(N, 1, 0.5),
-    SMC = c(rep(1, N/2), rep(0, N/2))
-  ) %>%
-    mutate(arm = case_when(
-      PEV == 1 & SMC == 1 ~ 'vaxsmc',
-      PEV == 1 & SMC == 0 ~ 'vax',
-      PEV == 0 & SMC == 1 ~ 'smc',
-      TRUE ~ 'none'))
+  # metadata_child <- data.frame(
+  #   rid = 1:N,
+  #   vaccination_day = vax_day,
+  #   PEV = rbinom(N, 1, 0.5),
+  #   SMC = c(rep(1, N/2), rep(0, N/2))
+  # ) %>%
+  #   mutate(arm = case_when(
+  #     PEV == 1 & SMC == 1 ~ 'vaxsmc',
+  #     PEV == 1 & SMC == 0 ~ 'vax',
+  #     PEV == 0 & SMC == 1 ~ 'smc',
+  #     TRUE ~ 'none'))
+  metadata_child <- metadata_df %>%
+    filter(country==country_to_run) #%>%
+    # for now, filtering out those that didn't have 3rd vaccine 
+    # mutate(vaccination_day = ifelse(is.na(vaccination_day), -51, vaccination_day))
+  country_short <- str_sub(country_to_run, 1, 1)
   
   # Initialize parasitemia storage
   parasitemia_storage <- vector("list", length = trial_ts + burnin)
   
-  # Main simulation loop (adapted from your original)
+  susceptibles <- rep(TRUE, N)
+  
+  # Choose probability of biting for baseline transmission/seasonality depending on country 
+  p_bite <- if(country_to_run == 'BF') p_bite_bfa else p_bite_mli
+  
+  # Main simulation loop 
   for (t in 1:(trial_ts + burnin)) {
-    
-    # Choose probability of biting for baseline transmission/seasonality depending on country 
-    p_bite <- if(country == 'BF') p_bite_bfa else p_bite_mli
+    # message(str_glue("infection records has {nrow(infection_records)} entries"))
     
     # Determine number of new infectious bites on time t
     n_infectious_bites <- rpois(1, lambda = N * p_bite[t])
@@ -79,10 +93,50 @@ run_cohort_simulation <- function(params_row,
     
     # Sample random children to be bitten 
     bites <- sample(1:N, size = n_infectious_bites, prob = weights, replace = TRUE)
-    message('There are ', length(bites), " mosquito bites on time ", t, ' out of ', (trial_ts+burnin))
+    # message('There are ', length(bites), " mosquito bites on time ", t, ' out of ', (trial_ts+burnin))
     
     bit_kids <- unique(bites)
     message('There are ', length(bit_kids), " unique kids bitten on time ", t, ' out of ', (trial_ts+burnin))
+    
+    
+    # Update susceptibility vector so that recovered kids would be susceptible again
+    if (exists("infection_records") && nrow(infection_records) > 0 & !allow_superinfections) {
+      
+      # Filter to only records with actual detections
+      detected_records <- infection_records[!is.na(infection_records$detection_day), ]
+      
+      if (nrow(detected_records) > 0) {
+        # Get the most recent detection for each person
+        most_recent <- detected_records %>%
+          group_by(rid) %>%
+          slice_max(detection_day, n = 1) %>%  # Get most recent detection_day for each rid
+          ungroup()
+        
+        # Find who should recover today (day t)
+        recovery_today <- (most_recent$recovery_day + burnin) == t # add burnin because the recovery day calculation is the external time but for this siulation we want ot know if on time t they could be infected
+        # if the recovery day equals today then these are the ones that are newly susceptible 
+        if (any(recovery_today)) {
+          recovering_kids <- most_recent$rid[recovery_today]
+          susceptibles[recovering_kids] <- TRUE
+        }
+
+      }
+      }
+    
+    # Avoid superinfection -- if a kid is bitten and they aren't susceptible they are not added to the list of bit_kids
+    bit_kids_t <- c()
+    j <- 1
+    for (kid in bit_kids){
+      if (susceptibles[kid]){ 
+        bit_kids_t[j] <- kid # Add this kid to the filtered list if the kid is susceptible
+        
+        j <- j + 1 # move to the next position in the filtered list 
+      }
+    }
+    message(str_glue('{length(bit_kids_t)} / {length(bit_kids)} bit kids were susceptible'))
+    bit_kids <- bit_kids_t
+    
+    # print(table(susceptibles))
     
     if(length(bit_kids) > 0) {
       # Estimate antibody level for each bitten child 
@@ -92,20 +146,25 @@ run_cohort_simulation <- function(params_row,
         SMC_vec <- rep(0, length(bit_kids))
         # these both need a placeholder value (won't matter if PEV = 0 or SMC = 0)
         t_since_vax_vec <- rep(0, length(bit_kids))
+        t_toboost1_vec <- rep(500, length(bit_kids))
+        t_toboost2_vec <- rep(1000, length(bit_kids))
       } else {
         
-        # Vectorized intervention calculations
         kid_metadata <- metadata_child[metadata_child$rid %in% bit_kids, ]
         
-        # Create named vectors
+        # Create named vectors of PEV, SMC statuses and vaccination days 
         pev_lookup <- setNames(kid_metadata$PEV, kid_metadata$rid)
         smc_lookup <- setNames(kid_metadata$SMC, kid_metadata$rid)
         vax_day_lookup <- setNames(kid_metadata$vaccination_day, kid_metadata$rid)
+        t_toboost1_lookup <- setNames(kid_metadata$t_to_boost1, kid_metadata$rid)
+        t_toboost2_lookup <- setNames(kid_metadata$t_to_boost2, kid_metadata$rid)
         
         # Map to bit_kids order
         PEV_vec <- pev_lookup[as.character(bit_kids)]
         SMC_vec <- smc_lookup[as.character(bit_kids)]
-        t_since_vax_vec <- (t - burnin) - vax_day_lookup[as.character(bit_kids)]
+        t_since_vax_vec <- (t - burnin) - vax_day_lookup[as.character(bit_kids)] # here, a - value of vaxdaylookup indicates vaccination before the follow-up  
+        t_toboost1_vec <- t_toboost1_lookup[as.character(bit_kids)]
+        t_toboost2_vec <- t_toboost2_lookup[as.character(bit_kids)]
         
         # Get intervention status
         message('PEV:', PEV_vec)
@@ -121,11 +180,13 @@ run_cohort_simulation <- function(params_row,
         season_start_day = season_start_day,  # Parameter from sweep
         season_length = season_length,        # Parameter from sweep
         smc_interval = smc_interval,          # Parameter from sweep
-        rid = bit_kids
+        rid = bit_kids,
+        t_toboost1 = t_toboost1_vec,
+        t_toboost2 = t_toboost2_vec
       )
       
       # Find time length until end of follow-up to run infection sim  
-      tt_until_end_cohort <- seq(0, ((trial_ts + burnin) - t)/divide, by = tstep)
+      tt_until_end_cohort <- seq(0, 100, by = tstep)#seq(0, ((trial_ts + burnin) - t)/divide, by = tstep)
       
       outputs <- pmap(params_df, 
                       function(PEV_on, 
@@ -135,7 +196,9 @@ run_cohort_simulation <- function(params_row,
                                season_start_day, 
                                season_length, 
                                smc_interval, 
-                               rid) {
+                               rid,
+                               t_toboost1,
+                               t_toboost2) {
                         result <- run_process_model(
                           PEV_on = PEV_on,
                           SMC_on = SMC_on,
@@ -147,7 +210,9 @@ run_cohort_simulation <- function(params_row,
                           infection_start_day = infection_start_day,# external time that infection begins 
                           season_start_day = season_start_day,# day of season start relative to Jan 1 of external year 
                           season_length = season_length,# ~4 months (120 days)
-                          smc_interval = smc_interval# how often are SMC rounds (days)
+                          smc_interval = smc_interval,# how often are SMC rounds (days)
+                          tboost1 = t_toboost1,
+                          tboost2 = t_toboost2
                         )
                         
                         result$trajectory$rid <- rid
@@ -158,17 +223,29 @@ run_cohort_simulation <- function(params_row,
       # Vectorized data frame creation for infection records
       new_records <- data.frame(
         time_ext = t - burnin,                                                      # external cohort time (t is the cohort time, then we wnat to scale to be + if after burnin)
-        rid = bit_kids,                                                        # number of bit kids
+        rid = paste0(country_short, bit_kids),                                                        # number of bit kids
         infectious_bite_day = (t - burnin) - t_liverstage,                          # bitten on day t, then assuming the liver stage takes t_liverstage days
         BSinfection_day = (t - burnin),                                             # after liver stage, the BS begins #+ t_liverstage
         threshold_day = sapply(outputs, function(x) x$threshold_day),               # days since BS starts that threshold is reached
         detection_day = (t - burnin) + sapply(outputs, function(x) x$threshold_day),# day in cohort simulation that threshold is reached, threshold is the day since BS infection that reaches threshold #+ t_liverstage 
         t_toreach_threshold = sapply(outputs, function(x) x$threshold_day) + t_liverstage,   # time to reach threshold value / detection since the bite  
         vaccination_day = if(t < burnin) rep(vax_day, length(bit_kids)) else kid_metadata$vaccination_day,   # day of vaccination relative to the start of follow-up (day 0 external time)
-        prob_bite = p_bite[t]
+        prob_bite = p_bite[t],
+        recovery_day = ((t - burnin) + sapply(outputs, function(x) x$threshold_day)) + 12 # day that the child would be 'recovered' if we assume that a child is treated and has a period of prophylaxis for 12 days after detection day 
+                                                                      # (90% at 12 days in paper but here, assuming 100% for 12 days) after the day of treatment and that all infectiosn are treated with AL 10.1038/ncomms6606
       ) 
       
       infection_records <- rbind(infection_records, new_records)
+      
+      # Only update the susceptibility vector if we are not allowing superinfections 
+      if(!allow_superinfections){
+        # mark as non-susceptible only those who have a valid detection_day
+        detected_kids <- bit_kids[!is.na(new_records$detection_day)]
+        # print(detected_kids)
+        if (length(detected_kids) > 0) {
+          susceptibles[detected_kids] <- FALSE
+        } 
+      }
       
       # Vectorized parasitemia storage creation
       parasitemia_data <- map2(outputs, bit_kids, function(output, kid) {
@@ -198,7 +275,8 @@ run_cohort_simulation <- function(params_row,
     group_by(rid, day1_BSinfection) %>%
     mutate(
     #   child_dayinf = paste0(rid, ", day ", day1_BSinfection),
-      det = ifelse(!is.na(detection_day), 1, 0)
+      det = ifelse(!is.na(detection_day), 1, 0),
+      country = country_to_run
     ) 
     
     # keep a small sample of the parasitemia dataset
@@ -211,7 +289,7 @@ run_cohort_simulation <- function(params_row,
   
   # Process infection records
   child_counts <- metadata_child %>%
-    distinct(rid, arm) %>%
+    distinct(rid, arm, country) %>%
     count(arm, name = "children_in_group")
   
   infection_records_final <- infection_records %>%
@@ -350,18 +428,26 @@ save_simulation_outputs <- function(results, output_dir) {
 }
 
 # Main function to run parameter sweep
-run_parameter_sweep <- function(params_df, base_inputs, output_dir = "simulation_outputs", 
+run_parameter_sweep <- function(metadata_df, 
+                                parameters_df, 
+                                base_inputs, 
+                                output_dir = "simulation_outputs", 
                                 save_outputs = TRUE,
-                                parallel = FALSE, n_cores = NULL) {
+                                parallel = FALSE, n_cores = NULL,
+                                allow_superinfections = TRUE) {
   
   if (!dir.exists(output_dir)) {
     dir.create(output_dir, recursive = TRUE)
   }
   
+  parameters_df$sim_id <- paste0("sim_",
+                   "SMCkill", parameters_df$max_SMC_kill_rate,
+                   "_SMCdecay", parameters_df$SMC_decay,
+                   "_season", parameters_df$season_start_day)
   # Save parameter grid
-  write.csv(params_df, file.path(output_dir, "parameter_grid.csv"), row.names = FALSE)
+  saveRDS(parameters_df, file.path(output_dir, "parameter_grid.rds"))
   saveRDS(base_inputs, file.path(output_dir, "base_inputs.rds"))
-  
+  cyphr::encrypt(saveRDS(metadata_df, file.path(output_dir, "metadata_df.rds")), key)
   
   if (parallel) {
     # if (is.null(n_cores)) {
@@ -402,28 +488,14 @@ run_parameter_sweep <- function(params_df, base_inputs, output_dir = "simulation
     
     results2 <- list()
     for (i in 1:nrow(params_df)) {
-      results2[[i]] <- run_cohort_simulation(params_df[i, ], base_inputs, output_dir,
-                                            save_outputs = TRUE)#results[[i]] <- 
+      results2[[i]] <- run_cohort_simulation(metadata_df, parameters_df[i, ], base_inputs, output_dir,
+                                            save_outputs = TRUE,
+                                            allow_superinfections = allow_superinfections)#results[[i]] <- 
       
-      message("done simulation ", i, " of ", nrow(params_df))
+      message("done simulation ", i, " of ", nrow(parameters_df))
       
-      # Calculate log-likelihood and append it to with params_df
     }
   }
-  
-  # # Save summary results
-  # summary_results <- map_dfr(results, function(x) {
-  #   data.frame(
-  #     sim_id = x$sim_id,
-  #     x$parameters,
-  #     n_detectable = sum(x$infection_records$detectable, na.rm = TRUE),
-  #     prop_detectable = mean(x$infection_records$detectable, na.rm = TRUE)
-  #   )
-  # })
-  # 
-  # write.csv(summary_results, file.path(output_dir, "summary_results.csv"), row.names = FALSE)
-
-  message("Parameter sweep complete! Results saved to: ", output_dir)
   
   return(list(
     results2
